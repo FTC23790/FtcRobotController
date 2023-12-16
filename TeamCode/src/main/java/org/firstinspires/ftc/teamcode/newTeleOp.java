6f47395034df5be838a929033ecb1ca7fa28ef7c
Ride the Robot <ridetherobot8559@gmail.com>
2023-12-16 11:31:59 -0500

    added arm and wrist servos to newTeleOp, reversed dc motors 0, 2, and 3, and changed the driver controller to gamepad1
    NOTE: SERVOS HAVE NOT BEEN TESTED YET!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -26,8 +27,11 @@ public class newTeleOp extends LinearOpMode {
 
     private DcMotor linearSlideMotor = null;
 
+    private Servo armJointServo = null;
+    private Servo wristJointServo = null;
+
     double drive_speed_multiplier = 0.75;
-    double lift_speed_multiplier = 0.4; //linear slide speed
+    double lift_speed_multiplier = 0.75; //linear slide speed
 
     @Override
     public void runOpMode() {
@@ -38,14 +42,16 @@ public class newTeleOp extends LinearOpMode {
         backLeft3   = hardwareMap.get(DcMotor.class, "backleft3");
 
         linearSlideMotor  = hardwareMap.get(DcMotor.class, "linearslide");
+        armJointServo = hardwareMap.get(Servo.class, "armJointMotor");
+        wristJointServo = hardwareMap.get(Servo.class, "wristJointMotor");
 
 
 
         //set direction for the motors
-        frontLeft0.  setDirection(DcMotor.Direction.FORWARD);
+        frontLeft0.  setDirection(DcMotor.Direction.REVERSE);
         frontRight1. setDirection(DcMotor.Direction.FORWARD);
-        backRight2.  setDirection(DcMotor.Direction.FORWARD);
-        backLeft3.   setDirection(DcMotor.Direction.FORWARD);
+        backRight2.  setDirection(DcMotor.Direction.REVERSE);
+        backLeft3.   setDirection(DcMotor.Direction.REVERSE);
 
         linearSlideMotor.    setDirection(DcMotor.Direction.REVERSE);
 
@@ -82,10 +88,10 @@ public class newTeleOp extends LinearOpMode {
             //for testing direction of motors
             //comment out if not testing
             /*
-            frontLeftPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad2
-            backLeftPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad2
-            frontRightPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad2
-            backRightPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad2
+            frontLeftPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad1
+            backLeftPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad1
+            frontRightPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad1
+            backRightPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad1
             */
 
 
@@ -95,9 +101,20 @@ public class newTeleOp extends LinearOpMode {
             backLeft3.setPower(drive_speed_multiplier*backLeftPower);
 
 
-            double lift_power = gamepad1.left_bumper ? 1.0 : 0.0; // linear slide speed: if left bumper pressed, speed = 1 else speed = 0
-            lift_power = gamepad1.right_bumper ? -1.0 : lift_power; // right bumper pressed, speed = -1
+            double lift_power = gamepad2.left_bumper ? 1.0 : 0.0; // linear slide speed: if left bumper pressed, speed = 1 else speed = 0
+            lift_power = gamepad2.right_bumper ? -1.0 : lift_power; // right bumper pressed, speed = -1
+            //to do: make sure that right and left bumper cant be pressed at the same time
             linearSlideMotor.setPower(lift_speed_multiplier * lift_power);
+
+            double arm_joint_position = gamepad2.dpad_up ? 0.25 : 0.0; // arm angle: if d-pad up button pressed, position = 0.25(180) else position = 0
+            arm_joint_position = gamepad2.dpad_down ? 0 : arm_joint_position; // d-pad down button pressed, position =  0 degrees
+            armJointServo.setPosition(arm_joint_position);
+
+            double wrist_joint_position = gamepad2.a ? 0.25 : 0.0; // wrist angle: if a up button pressed, position = 0.25(180) else position = 0
+            wrist_joint_position = gamepad2.x ? 0 : wrist_joint_position; // x down button pressed, position =  0 degrees
+            armJointServo.setPosition(wrist_joint_position);
+
+
         }
 
     }
